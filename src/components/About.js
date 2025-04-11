7d6418b32d2a3db5780b46c9bcdb72b3958a120e
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-04-11 19:36:58 +0300

    pass tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
+// src/components/About.js
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
-}
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello! I am a passionate web developer, always eager to learn new
+        technologies and improve my skills.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
 
 export default About;
